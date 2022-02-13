dfeda33664ab363d2980f0ff71580125549d3d3f
Bharti kumari <bhartikumari20@navgurukul.org>
2022-02-13 10:43:45 +0530

    pockeman project

---
+++ final.py
@@ -0,0 +1,31 @@
+# l = [0,0,0,0]
+# list = []
+
+# mi = 0
+# ma = 0
+# x = 1
+# c = 0
+# if len(l) == 1:
+#     print(l[0], l[0])
+# elif len(l) == 2:
+#     print(min(l),min(l))
+#     print(min(l), max(l))
+# else:
+#     for i in l:
+#         if c < len(l)-1:
+#             if mi == 0 and ma == 0:
+#                 mi = min(l)
+#                 ma = max(l)
+#                 print(mi, mi)
+#                 c = c+1
+#             else:
+#                 if ma-x in l:
+#                     print(mi, ma-x)
+#                     c = c+1
+#                     x = x+1
+#         else:
+#             break
+#     print(mi, ma)
+#     print(mi, ma)
+    
+
---
+++ main.py
@@ -0,0 +1,47 @@
+how_many_p = int(input('Enter how many pockman do you wnat to catch?  '))
+powers = []
+i = 0
+while i < how_many_p:
+    pockman_p = int(input('Enter pockman '))
+    powers.append(pockman_p)
+    i = i+1
+min_p = min(powers)
+max_P = max(powers)
+
+if len(powers) == 1:
+    print(powers[0], powers[0])
+elif len(powers) == 2:
+    print(min(powers),min(powers))
+    print(min(powers), max(powers))
+else:
+    # dic_of_powers = {}
+    x = 1
+    count2 = len(powers)
+    count = 3
+    print(min_p, min_p)
+    # dic_of_powers[min_p] = min_p
+    while True:
+        if min_p == 0 and max_P == 0:
+            if count2!= 1:
+                print(min_p, max_P)
+                count2 = count2 -1
+            else:
+                break
+        elif count != len(powers):
+            if max_P-x in powers:
+                count+=1
+                print(min_p, max_P-x )
+                # dic_of_powers[min_p] = max_P
+                
+            x = x+1
+        else:
+            count = 'elif'
+            break
+    if count == 'elif':
+        print(min_p, max_P)
+        print(min_p, max_P)
+        # dic_of_powers[min_p] = max_P
+        # dic_of_powers[min_p] = max_P
+# print(dic_of_powers)
+
+
